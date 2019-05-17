fe8bf4caab19c46091a226dc798340c0976c0e2d
Kyle Kitlinski <kylekitlinski@gmail.com>
2019-05-17 15:36:42 -0400

    update for test key

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config = {
-    snipcartPublicKey: "YTJhODQ0ZmQtZWQxZi00ZjUxLTg1MWItOTMyMDAzZDQ0MzMzNjM2OTA2OTkzMDk5MTg4NTA5"
+    snipcartPublicKey: "YzRkZDU4NTUtMzQ1MC00NTA3LWIzNDUtNTNiZjI0MmMyYTg3NjM2OTA2OTkzMDk5MTg4NTA5"
     
 }
